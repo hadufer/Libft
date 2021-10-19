2e9ae12ea5bac1d9f42d73f2f679db15a6c423de
Hassan Dufer <hadufer@c1r2p7.42nice.fr>
2021-10-19 11:29:23 +0200

    everything patched according to tripouille and libft-unit-tester

--- ft_atoi.c
+++ ft_atoi.c
@@ -39,7 +39,7 @@ int	ft_atoi(const char *str)
 	while (str[i] == '\t' || str[i] == '\n' || str[i] == '\v'
 		|| str[i] == '\f' || str[i] == '\r' || str[i] == ' ')
 		i++;
-	while (str[i] == '+' || str[i] == '-')
+	if (str[i] == '+' || str[i] == '-')
 	{
 		if (str[i] == '-')
 			tmp[0] *= -1;
